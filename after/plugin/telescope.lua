2dc4318b630982f31c490b90daa94d7a40b52002
Enzoerb <enzoerbert@gmail.com>
2025-05-20 21:10:47 -0300

    setup regex search with <leader>rs

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,9 +2,11 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<Leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
-vim.keymap.set('n', '<Leader>ps', function() 
+vim.keymap.set('n', '<leader>rs', builtin.live_grep, { desc = 'Telescope live grep (regex)' })
+vim.keymap.set('n', '<Leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = 'Telescope grep string' })
+
 
 -- Map <Leader>, to show recently opened files
 vim.keymap.set('n', '<Leader>,', function()
